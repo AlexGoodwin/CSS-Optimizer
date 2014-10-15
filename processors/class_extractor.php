d75344319b61de7911eae2930aed5cf299222c34
Stephen Thoma <sthoma4@gmail.com>
2014-10-15 16:26:17 -0600

    Add tag extraction.

--- processors/class_extractor.php
+++ processors/class_extractor.php
@@ -1,16 +1,34 @@
 <?php
+function map_trim($val, $del) {
+  return trim($val, $del);
+}
+
 function extractIdentifiers($html) {
   // Accepts a string containing HTML which will be parsed to
   // retrieve ids, classes, and DOM elements used.
-  $res = array();
+  $id_res = array();
+  $tag_res = array();
 
-  // Extract classes and ids.
+  // Extract classes and ids
   $pattern = "/(class|id) ?= ?(\"|').+?(\"|')/";
-  preg_match_all($pattern, $html, $matches);
-  foreach ($matches[0] as $match) {
+  preg_match_all($pattern, $html, $id_matches);
+  // Strip quotes from results
+  foreach ($id_matches[0] as $match) {
     $clean = explode("\"", $match)[1];
-    array_push($res, $clean);
+    array_push($id_res, $clean);
   }
+
+  // Extract HTML tags
+  $pattern = "/<.+?( |>)/";
+  preg_match_all($pattern, $html, $tag_matches);
+  // Strip spaces and brackets
+  foreach ($tag_matches[0] as $match) {
+    $clean = trim($match, '</> ');
+    array_push($tag_res, $clean);
+  }
+
+  // Merge results arrays and return de-duped array of results
+  $res = array_merge($id_res, $tag_res);
   return array_unique($res);
 }
 ?>
