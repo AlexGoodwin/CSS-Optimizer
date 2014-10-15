52f1327ff82a1f1da71edf0405ea933f80daa5d9
Alex Goodwin <alexander.goodwin@colorado.edu>
2014-10-15 14:17:58 -0600

    whitespace removed, bitches

--- functions.php
+++ functions.php
@@ -1,18 +1,22 @@
-  
 <?php
 function remove_whitespace($file) {
-        
-        $feed = file_get_contents($file);
-        return trim($feed);
+    
+    $feed = file_get_contents($file);
+    
+    echo "<script>console.log('testing');</script>";
 
+	$trimmed = preg_replace('/\s+/', '', $feed);
+	
+	file_put_contents($file, $trimmed);
+
+    return $trimmed;
 }
 function remove_comments($file){
 
 	$temp = $file;
 	$temp = preg_replace('!/\*.*?\*/!s', '', $temp);
 	$temp = preg_replace('/\n\s*\n/', "\n", $temp);
+
 	return $temp;
-
-
 }
 ?>
