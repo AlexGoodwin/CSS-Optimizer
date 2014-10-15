32d91d54643ce27da9532c827640fcd3c0d63aeb
Stephen Thoma <sthoma4@gmail.com>
2014-10-15 15:14:47 -0600

    Make results be unique.

--- processors/class_extractor.php
+++ processors/class_extractor.php
@@ -1,12 +1,17 @@
 <?php
 function extractIdentifiers($html) {
-        $res = array();
-        $pattern = "/(class|id) ?= ?(\"|').+?(\"|')/";
-        preg_match_all($pattern, $html, $matches);
-        foreach ($matches[0] as $match) {
-          $clean = explode("\"", $match)[1];
-          array_push($res, $clean);
-        }
-        return $res;
+  // Accepts a string containing HTML which will be parsed to
+  // retrieve ids, classes, and DOM elements used.
+  $res = array();
+
+  // Extract classes and ids.
+  $pattern = "/(class|id) ?= ?(\"|').+?(\"|')/";
+  preg_match_all($pattern, $html, $matches);
+  foreach ($matches[0] as $match) {
+    $clean = explode("\"", $match)[1];
+    array_push($res, $clean);
+  }
+  return array_unique($res);
 }
 ?>
+
